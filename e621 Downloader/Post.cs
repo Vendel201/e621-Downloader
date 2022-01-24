e3afc60fb307b4c1069607391c25e6929c2b514c
Vendell <vendel2525@gmail.com>
2022-01-24 19:44:32 +0100

    Rewrote the JSON handling to be actual JSON Deserialization and not just bruteforcing strings

---
+++ e621 Downloader/Post.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e621Downloader
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class PostFile
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        public string ext { get; set; }
+        public int size { get; set; }
+        public string md5 { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Preview
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Alternates
+    {
+    }
+
+    public class Sample
+    {
+        public bool has { get; set; }
+        public int height { get; set; }
+        public int width { get; set; }
+        public string url { get; set; }
+        public Alternates alternates { get; set; }
+    }
+
+    public class Score
+    {
+        public int up { get; set; }
+        public int down { get; set; }
+        public int total { get; set; }
+    }
+
+    public class Tags
+    {
+        public List<string> general { get; set; }
+        public List<string> species { get; set; }
+        public List<string> character { get; set; }
+        public List<string> copyright { get; set; }
+        public List<string> artist { get; set; }
+        public List<object> invalid { get; set; }
+        public List<object> lore { get; set; }
+        public List<string> meta { get; set; }
+    }
+
+    public class Flags
+    {
+        public bool pending { get; set; }
+        public bool flagged { get; set; }
+        public bool note_locked { get; set; }
+        public bool status_locked { get; set; }
+        public bool rating_locked { get; set; }
+        public bool deleted { get; set; }
+    }
+
+    public class Relationships
+    {
+        public int? parent_id { get; set; }
+        public bool has_children { get; set; }
+        public bool has_active_children { get; set; }
+        public List<int> children { get; set; }
+    }
+
+    public class Post
+    {
+        public int id { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+        public PostFile file { get; set; }
+        public Preview preview { get; set; }
+        public Sample sample { get; set; }
+        public Score score { get; set; }
+        public Tags tags { get; set; }
+        public List<string> locked_tags { get; set; }
+        public int change_seq { get; set; }
+        public Flags flags { get; set; }
+        public string rating { get; set; }
+        public int fav_count { get; set; }
+        public List<string> sources { get; set; }
+        public List<int> pools { get; set; }
+        public Relationships relationships { get; set; }
+        public int? approver_id { get; set; }
+        public int uploader_id { get; set; }
+        public string description { get; set; }
+        public int comment_count { get; set; }
+        public bool is_favorited { get; set; }
+        public bool has_notes { get; set; }
+        public object duration { get; set; }
+    }
+
+    public class Root
+    {
+        public List<Post> posts { get; set; }
+    }
+}
--- e621 Downloader/Program.cs
+++ e621 Downloader/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -9,43 +10,13 @@ using System.Net;
 namespace e621Downloader
 {
     internal class Program
-    {
-
-        public static List<string> imagesJPG;
-        public static List<string> imagesJPGNew;
-        public static List<string> imagesPNG;
-        public static List<string> imagesPNGNew;
-        public static List<string> imagesGIF;
-        public static List<string> imagesGIFNew;
-        public static List<string> imagesWEBM;
-        public static List<string> imagesWEBMNew;
-
-        public static float fileIndex;
-
+    { 
         public static string query;
-
-        public static string newline;
 
         public static float downloadedCount;
 
-        public static WebClient client;
-
-        public static string path;
-
-        static Program()
-        {
-            imagesJPG = new List<string>();
-            imagesJPGNew = new List<string>();
-            imagesPNG = new List<string>();
-            imagesPNGNew = new List<string>();
-            imagesGIF = new List<string>();
-            imagesGIFNew = new List<string>();
-            imagesWEBM = new List<string>();
-            imagesWEBMNew = new List<string>();
-            newline = Environment.NewLine;
-            client = new WebClient();
-            path = File.ReadLines(string.Concat(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName), "\\config.txt")).First<string>();
-        }
+        public static WebClient client = new WebClient();
+        public static string path = File.ReadLines(string.Concat(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName), "\\config.txt")).First<string>();
 
         public static void Main()
         {
@@ -54,11 +25,13 @@ namespace e621Downloader
             Console.WriteLine("Example: limit=10&tags=cute+dragon");
             Console.Write("Query: ");
             query = Console.ReadLine();
+
             if (query.Contains("cub"))
             {
                 Console.WriteLine("");
                 Console.WriteLine("FBI OPEN UP");
             }
+
             Console.WriteLine("");
             HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(string.Concat("https://e621.net/posts.json?", query, "&login=Vendell&api_key=AadndrowG4dy4SoJFjpPGxdc"));
             httpWebRequest.UserAgent = "Vendell / e621 Bulk Downloader";
@@ -67,80 +40,29 @@ namespace e621Downloader
             {
                 using (Stream stream = response.GetResponseStream())
                 {
-                    JsonTextReader JsonReader = new JsonTextReader(new StringReader((new StreamReader(stream)).ReadToEnd()));
-                    while (JsonReader.Read())
+
+                    List<Post> originalTweets = new List<Post>();
+
+                    StreamReader reader = new StreamReader(stream, true);
+                    String responseString = reader.ReadToEnd();
+                    Root objects = JsonConvert.DeserializeObject<Root>(responseString);
+
+                    int count = objects.posts.Count;
+
+                    foreach (var post in objects.posts)
                     {
-                        if (JsonReader.Value == null)
-                        {
-                            continue;
-                        }
-                        string value = JsonReader.Value.ToString();
-                        if (value.Contains("https://static1.e621.net/data/") && value.Contains(".jpg") && !value.Contains("preview") && !value.Contains("sample"))
-                        {
-                            imagesJPG.Add(value);
-                        }
-                        if (value.Contains("https://static1.e621.net/data/") && value.Contains(".png") && !value.Contains("preview") && !value.Contains("sample"))
-                        {
-                            imagesPNG.Add(value);
-                        }
-                        if (value.Contains("https://static1.e621.net/data/") && value.Contains(".gif") && !value.Contains("preview") && !value.Contains("sample"))
-                        {
-                            imagesGIF.Add(value);
-                        }
-                        if (value.Contains("https://static1.e621.net/data/") && value.Contains(".webm") && !value.Contains("preview") && !value.Contains("sample"))
-                        {
-                            imagesWEBM.Add(value);
-                        }
+                        downloadedCount++;
+                        Console.Title = count + " / " + (count - downloadedCount).ToString();
+                        string filename = string.Concat(path, post.file.md5, ".", post.file.ext);
+                        client.DownloadFile(post.file.url, filename);
                     }
                 }
             }
-            imagesJPGNew = imagesJPG.Distinct<string>().ToList<string>();
-            foreach (string a in Program.imagesJPGNew)
-            {
-                Console.Title = string.Concat("E621 Bulk Downloader ", a);
-                fileIndex += 1f;
-                Console.WriteLine(a);
-                client.DownloadFile(a, string.Concat(path, a.Remove(0, 36)));
-            }
-            imagesJPG.Clear();
-            imagesPNGNew = imagesPNG.Distinct<string>().ToList<string>();
-            foreach (string a in imagesPNGNew)
-            {
-                Console.Title = string.Concat("E621 Bulk Downloader ", a);
-                fileIndex += 1f;
-                Console.WriteLine(a);
-                client.DownloadFile(a, string.Concat(path, a.Remove(0, 36)));
-            }
-            imagesPNG.Clear();
-            imagesGIFNew = imagesGIF.Distinct<string>().ToList<string>();
-            foreach (string a in imagesGIFNew)
-            {
-                Console.Title = string.Concat("E621 Bulk Downloader ", a);
-                fileIndex += 1f;
-                Console.WriteLine(a);
-                client.DownloadFile(a, string.Concat(path, a.Remove(0, 36)));
-            }
-            imagesWEBMNew = imagesWEBM.Distinct<string>().ToList<string>();
-            foreach (string a in imagesWEBMNew)
-            {
-                Console.Title = string.Concat("E621 Bulk Downloader ", a);
-                fileIndex += 1f;
-                Console.WriteLine(a);
-                client.DownloadFile(a, string.Concat(path, a.Remove(0, 36)));
-            }
+
             Console.Title = "E621 Bulk Downloader";
-            Program.downloadedCount = Program.imagesGIFNew.Count + Program.imagesJPGNew.Count + Program.imagesPNGNew.Count + Program.imagesWEBMNew.Count;
-            Console.WriteLine(string.Concat("You downloaded ", Program.downloadedCount, " images!"));
-            File.AppendAllText(string.Concat(Program.path, "\\queries.txt"), string.Concat(new object[] { DateTime.Now, " Query: ", Program.query, " / Downloaded: ", Program.downloadedCount, Program.newline }));
-            Program.imagesGIF.Clear();
-            Program.imagesGIFNew.Clear();
-            Program.imagesJPG.Clear();
-            Program.imagesJPGNew.Clear();
-            Program.imagesPNG.Clear();
-            Program.imagesPNGNew.Clear();
-            Program.imagesWEBM.Clear();
-            Program.imagesWEBMNew.Clear();
-            Program.downloadedCount = 0f;
+            Console.WriteLine(string.Concat("You downloaded ", downloadedCount, " images!"));
+            File.AppendAllText(string.Concat(path, "\\queries.txt"), string.Concat(new object[] { DateTime.Now, " Query: ", query, " / Downloaded: ", downloadedCount, "\n" }));
+            downloadedCount = 0;
             Console.WriteLine("");
             Console.WriteLine("Do you want to download more yiff?");
             Console.WriteLine("(Y/N)");
@@ -150,7 +72,7 @@ namespace e621Downloader
                 return;
             }
             Console.WriteLine("");
-            Program.Main();
+            Main();
         }
     }
 }
